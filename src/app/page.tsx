2229e8a69794309fae7f7af1de6c8954a9eccb14
Riyan Ramadhan <ramadhanriyan3@gmail.com>
2024-04-05 10:15:58 +0700

    add CI/CD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
   return (
     <>
-      <div>main</div>
+      <div onClick={redirect("/dashboard")}></div>
     </>
   );
 }
